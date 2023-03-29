22318f92a1c6c9b913ffb45db2cb3791fe241735
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2023-03-29 09:24:41 +0700

    membuat fungsi if dan else sebagai output di layar jika memenuhi syarat

--- Exercise_1_PD/Exercise_1_PD.cpp
+++ Exercise_1_PD/Exercise_1_PD.cpp
@@ -4,3 +4,10 @@ using namespace std;
 double ratarata(double a, double b) {
 	return (a + b) / 2;
 }
+
+string status(double t, double m) {
+	if (t = 70 || m = 80)
+		return "Diterima";
+	else
+		return "Di tolak";
+}
