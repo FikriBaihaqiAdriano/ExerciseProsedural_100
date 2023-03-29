d3730a39d958ad46118135b60249eb6b8f1d03de
FikriBaihaqiAdriano <fikribaihaqiadriano@gmail.com>
2023-03-29 09:34:31 +0700

    membuat prosedur input yang berisi Nama, Nilai Matematika dan Nilai Bahasa Inggris

--- Exercise_1_PD/Exercise_1_PD.cpp
+++ Exercise_1_PD/Exercise_1_PD.cpp
@@ -6,8 +6,26 @@ double ratarata(double a, double b) {
 }
 
 string status(double t, double m) {
-	if (t = 70 || m = 80)
+	if (t >= 70 || m >= 80)
 		return "Diterima";
 	else
 		return "Di tolak";
 }
+
+string Nama[20];
+int nilMTK[20], nilBing[20];
+float rataratanil[20];
+
+void input() {
+	for (int i = 0; i < 20; i++) {
+
+		cout << "Masukkan Nama : ";
+		cin >> Nama[i];
+		cout << "Masukkan Nilai Matematika : ";
+		cin >> nilMTK[i];
+		cout << "Masukkan Nilai Bahasa Inggris : ";
+		cin >> nilBing[i];
+
+	}
+}
+
